047fb5515035e6e87823059bbfed53f590e59fe3
amans392 <71714627+amans392@users.noreply.github.com>
2024-05-07 17:10:56 -0400

    reusing components

--- mtgcardviewer/src/Components/Bloglist.js
+++ mtgcardviewer/src/Components/Bloglist.js
@@ -1,9 +1,13 @@
+//current-video https://www.youtube.com/watch?v=-YpnB-zlkPU&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=12
 //props object in () of arrow function is associated with the blogs value from Home2
-const BlogList = (props) => {
+//destructuring the props by {blogs, title} also works
+
+const BlogList = ({ blogs, title}) => {
+//const BlogList = (props) => {
     //const blogs below grabs the blogs and storing them in the const
-    const blogs = props.blogs;
-    const title = props.title;
-    console.log(props,blogs)
+    //const blogs = props.blogs;
+    //const title = props.title;
+    //console.log(props,blogs)
     return ( 
         //cycling through the const blogs below
         <div className="blog-list">
