5cd7e18001243f45296ac8b1a85c7744ac765afb
amans392 <71714627+amans392@users.noreply.github.com>
2024-05-15 17:00:00 -0400

    CardList Props added
    CardList Props and Dependency added

--- mtgcardviewer/src/Components/Home.js
+++ mtgcardviewer/src/Components/Home.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+//next step see useFectch Component https://www.youtube.com/watch?v=qdCHEUaFhBk&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=17
+import { useState, useEffect } from "react";
 import CardList from "./Cardlist";
 const Home = () => {
     const [cards, setCards] = useState([
@@ -6,7 +7,19 @@ const Home = () => {
         {name: "Llanowar Elf", type: "Elf", cmc: 1, id: 2},
         {name: "Ghalta, Primal Hunger", type: "Dinosaur", cmc: 12, id:3},
     ]);
+    //going to get this name depenency set to the search bar results
+    //https://www.youtube.com/watch?v=jQc_bTFZ5_I&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=15
+    //const [name, setName] = useState()
 
+    //useEffect hook function runs everytime there is a re-render
+    //useful for things like fetching data
+    //added a dependency array [] only runs function after first initial render
+    //state changes won't run the function that has the depenency array
+    //values added so that it changes when the dependcy takes effect
+    useEffect(() => {
+        console.log("useEffect Ran")
+        console.log(cards);
+    }, []);
     //map method used to cycle through the array of items above
     //return statement outputs the template to the browser
     //takes the cards property set equal to the array of cards with their properties name, type and cmc
--- mtgcardviewer/src/Components/UseFetch.js
+++ mtgcardviewer/src/Components/UseFetch.js
@@ -1,17 +1,2 @@
-import { useState, useEffect } from "react"
-const useFetch = () => {
-    const URL = "https://api.magicthegathering.io/v1/cards"
-    const [data, setData] = useState(null) 
-
-    useEffect(() => {
-        fetch(URL)
-        .then(res => {
-            return res.json()
-        })
-        .then(data) => {
-            console.log(data)
-        }
-    }, []);
-
-}
-export default useFetch;
+//useFetch setup 
+// https://www.youtube.com/watch?v=qdCHEUaFhBk&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=17
