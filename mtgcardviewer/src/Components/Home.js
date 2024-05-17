a5d5cf5fe762e93b643ccd3811af3cae71388938
amans392 <71714627+amans392@users.noreply.github.com>
2024-05-17 10:27:11 -0400

    added a loading state prior to card display Data

--- mtgcardviewer/src/Components/Home.js
+++ mtgcardviewer/src/Components/Home.js
@@ -2,11 +2,15 @@
 import { useState, useEffect } from "react";
 import CardList from "./Cardlist";
 const Home = () => {
-    const [card, setCards] = useState([
+    //added cards and setcards state values
+    const [card, setCards] = useState(null);
         // {name: "Strangleroot Geist", type: "spirit", cmc: 2, id: 1},
         // {name: "Llanowar Elf", type: "Elf", cmc: 1, id: 2},
         // {name: "Ghalta, Primal Hunger", type: "Dinosaur", cmc: 12, id:3},
-    ]);
+    //adding a conditional loading message isLoading, setLoading
+    const [isLoading, setIsLoading] = useState(true);
+    
+
     //going to get this name depenency set to the search bar results
     //https://www.youtube.com/watch?v=jQc_bTFZ5_I&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=15
     //const [name, setName] = useState()
@@ -21,10 +25,14 @@ const Home = () => {
     const URL = "https://api.magicthegathering.io/v1/cards"
     //adding fetch request under useEffect hook
     useEffect(() => {
-        console.log("useEffect Ran")
-        handleFetchData();
-        //sending git request to the API
-        //returns a prommise
+        //adding a setTimout function with a 1 second delay to display loading before card data
+        setTimeout(() => {
+            console.log("useEffect Ran")
+            handleFetchData();
+            setIsLoading(false);
+            //sending git request to the API
+            //returns a prommise
+        }, 500)
 
         }, [])
         //creates a data fetch const variable using async
@@ -32,6 +40,7 @@ const Home = () => {
             const response = await fetch(URL);
             //stores the json data as the setCards state value
             setCards(await response.json());
+            
         }
     //map method used to cycle through the array of items above
     //return statement outputs the template to the browser
@@ -41,8 +50,11 @@ const Home = () => {
     // then passed in the props parameter to CardList
     // then in CardList, created const variable cards set equal to passed in props.cards value
     //now the component can see and use the cards variable props in the array
+    //logial && added so that card state will be null unless the data is available from CardLIst to load
+    //added a logical && for loading message while data state is being loaded
     return ( 
         <div className="Home">
+            { isLoading && <div> Loading...</div> }
             {card && <CardList card={card}></CardList>}
         </div>
      );
