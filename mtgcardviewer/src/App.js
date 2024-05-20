fa8ed8004c9fc84182e8dc1b930f4fcfbb93e5d6
amans392 <71714627+amans392@users.noreply.github.com>
2024-05-20 16:22:09 -0400

    API and Search Filter to results
    Added data fetching functionality and filtering of data from results variable

--- mtgcardviewer/src/App.js
+++ mtgcardviewer/src/App.js
@@ -1,6 +1,6 @@
 //tutorial current time: https://youtu.be/NbTrGcz4DW8?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&t=195
 
-
+import { useState } from 'react';
 //import react hooks below
 import Navbar from './Components/Navbar';
 import FetchData from "./Components/FetchData";
@@ -10,12 +10,17 @@ import SearchBar from './Components/SearchBar';
 //Navbar and Home components nested in the return statement below app
 //using <Navbar /> and <Home />
 function App() {
-  
+
+//state variable results created for search bar
+//
+const [results, setResults] = useState([]);
+
   return (
     <div className="app">
       <Navbar />
       <div className='content'>
-      <SearchBar></SearchBar>
+      <SearchBar setResults={setResults}></SearchBar>
+      <div>SearchResults</div>
       <Home></Home>
       
         
--- mtgcardviewer/src/Components/SearchBar.js
+++ mtgcardviewer/src/Components/SearchBar.js
@@ -1,18 +1,62 @@
 //Creating a search bar with API calls current-time
-//https://youtu.be/sWVgMcz8Q44?t=659
+//https://youtu.be/sWVgMcz8Q44?t=743
 
 //creating a serach bar component
 //opened terminal and ran npm install react-icons for usage of icons
 import React from "react";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css"
 
-const SearchBar = () => {
+
+//in SearchBartaking function,  
+const SearchBar = ({setResults}) => {
+    //created a state varialbes input and set input equal to useState('') empty string
+    const [input, setInput ] = useState('')
+
+    // FetchData variable below is used for fetching data from external API
+    // fetch function is asynch, then chain a .then then convert the response into the json format
+    const FetchData = (value) => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((json) => {
+            //filtering the json data so it matches what's written in input
+            //checks data in input field against names and returns false if no match
+            const results = json.filter((user) => {
+                //
+                return (
+                    value &&
+                    user &&
+                    user.name &&
+                    user.name.toLowerCase().includes(value) )
+            });
+
+            //console.log(results);
+            //when input is changed
+            //will fetch the data from API
+            //filter based on text in input
+            //set results variable to what we get back
+            //and store it in the results variable below
+            setResults(results)
+            //logging the json data
+            console.log(json)
+        });
+    }
+
+    const handleChange = (value) => {
+        setInput(value)
+        FetchData(value)
+    }
     return ( 
         //created class input-wrapper for search bar
+        //created an input field placeholder with a value =input variable then onChange
+        //akes the (e) event set input variable to value inside input element with e.target.value
+        //this stores what is typed in search bar in input variable.
+        //handleChange variable defined above used in input field so that when text changes, it makes request to fetchData API
     <div className="input-wrapper">
     <FaSearch id="search-icon">  </FaSearch>
-    <input placeholder="Type to search..."></input>
+    
+    <input placeholder="Type to search..." value={input} onChange={(e) => handleChange(e.target.value)} ></input>
     
     </div> 
     
